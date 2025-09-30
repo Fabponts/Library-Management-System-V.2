92fc4fc74f9142e64a4f0c0aedeac58d6147301e
Fabpont <fabriciopontes02.fa@gmail.com>
2025-09-30 17:29:37 -0300

    feat(userservice): add operations system in the class

--- src/main/java/com/example/library/services/UserService.java
+++ src/main/java/com/example/library/services/UserService.java
@@ -1,7 +1,33 @@
 package com.example.library.services;
 
+import com.example.library.models.User;
+import com.example.library.repositories.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
+    private final UserRepository  userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+    public List<User> showAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findById(Long id){
+        return userRepository.findById(id);
+    }
+
+    public void deleteUserByid(Long id){
+        userRepository.deleteById(id);
+    }
+
 }
