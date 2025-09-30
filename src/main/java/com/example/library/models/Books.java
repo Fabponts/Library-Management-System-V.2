a6e80064ec6d8bc94928d5823f9b332ee9590d4b
Fabpont <fabriciopontes02.fa@gmail.com>
2025-09-30 10:51:58 -0300

    feat(books): add entity and database mapping

--- src/main/java/com/example/library/models/Books.java
+++ src/main/java/com/example/library/models/Books.java
@@ -1,5 +1,61 @@
 package com.example.library.models;
 
-public class Books {
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "TB_BOOKS")
+public class Books {
+     @Id
+     @GeneratedValue(strategy = GenerationType.AUTO)
+     private Integer id;
+     private String title;
+     private String author;
+     private String publisher;
+     private String genre;
+     private String description;
+
+     //Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
+
