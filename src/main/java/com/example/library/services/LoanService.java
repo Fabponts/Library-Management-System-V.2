8e59c76452bab3f3bd8a725bab79d065832664e8
Fabpont <fabriciopontes02.fa@gmail.com>
2025-10-07 11:53:07 -0300

    feat(loanservice): add a business logic in loanservice class

--- src/main/java/com/example/library/services/LoanService.java
+++ src/main/java/com/example/library/services/LoanService.java
@@ -1,4 +1,35 @@
 package com.example.library.services;
 
+import com.example.library.models.Loan;
+import com.example.library.repositories.LoanRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class LoanService {
+    private final LoanRepository loanRepository;
+
+    public LoanService(LoanRepository loanRepository) {
+        this.loanRepository = loanRepository;
+    }
+
+    //Get all Loans
+    public List<Loan> getAllLoan(){
+        return loanRepository.findAll();
+    }
+
+    public Optional<Loan> findLoanById(Integer id){
+    return loanRepository.findById(id);
+    }
+
+    public Loan saveLoan(Loan loan){
+        return loanRepository.save(loan);
+    }
+
+    public void deleteLoanById(Integer id ){
+        loanRepository.deleteById(id);
+    }
+
 }
