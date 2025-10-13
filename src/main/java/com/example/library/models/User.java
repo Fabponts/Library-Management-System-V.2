20044490fc574814b8ff5d418afcc1d9e56811c3
Fabpont <fabriciopontes02.fa@gmail.com>
2025-10-13 18:06:53 -0300

    feat(user):add new getters and setters

--- src/main/java/com/example/library/models/User.java
+++ src/main/java/com/example/library/models/User.java
@@ -29,15 +29,31 @@ public class User {
     @OneToMany(mappedBy = "user")
     private List<Book> books = new ArrayList<>();
 
-    public void setEmail(String email){
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
     }
 
-    public String getEmail(){
-        return this.email;
+    public String getPassword() {
+        return password;
     }
 
-    public Long getId(){
-        return this.id;
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
     }
 }
